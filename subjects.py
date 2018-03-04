0d659cdc0d593c03dab30855d6f66dae15b5d4d4
Sadjad Anzabi Zadeh <sanzabizadeh@uiowa.edu>
2018-03-03 23:39:19 -0600

    Added Snake subject and changed Subject class to have "state" property and MNKBoard class to have "clear_piece" method.

--- subjects.py
+++ subjects.py
@@ -57,7 +57,18 @@ class Subject:
         self._agent_list = {}
 
     @property
+    def state(self):
+        '''
+        Returns the state.
+        '''
+        raise NotImplementedError
+
+    @property
     def is_terminated(self):
+        pass
+
+    @property
+    def possible_actions(self):
         pass
 
     def register(self, agent_name):
@@ -158,19 +169,27 @@ class MNKBoard:
         except KeyError:
             self._board[kwargs['row']*self._n + kwargs['column']] = player
 
+    def clear_piece(self, **kwargs):
+        '''
+        Clears a location on the board.
+        \nArguments:
+        \n    index: If provided, the piece is set using 'index'. Index starts from 0 and assumes the board to be a list.
+        \n    row: The row in which the piece is set.
+        \n    column: The column in which the piece is set.
+        '''
+        if not kwargs:
+            raise ValueError('No row-column pair or index found.')
+        try:
+            self._board[kwargs['index']] = 0
+        except KeyError:
+            self._board[kwargs['row']*self._n + kwargs['column']] = 0
+
     @property
     def state(self):
         '''
         Returns the state of the board as a list.
         '''
         return self._board
-
-    @property
-    def possible_actions(self):
-        '''
-        Returns a list of indexes of empty squares.
-        '''  
-        return list(self.get_action_set())
 
     def get_board(self, format_='vector'):
         '''
@@ -264,6 +283,13 @@ class MNKGame(MNKBoard, Subject):
         if self.board_status is None:
             return False
         return True
+
+    @property
+    def possible_actions(self):
+        '''
+        Returns a list of indexes of empty squares.
+        '''  
+        return list(self.get_action_set())
 
     def register(self, player_name):
         '''
@@ -420,6 +446,123 @@ class MNKGame(MNKBoard, Subject):
 
         return None
 
+
+import curses
+from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
+from random import randint
+
+class Snake(MNKBoard, Subject):
+    def __init__(self, **kwargs):
+        try:
+            self._m = kwargs['m']
+        except KeyError:
+            self._m = 10
+        try:
+            self._n = kwargs['n']
+        except KeyError:
+            self._n = 10
+
+        MNKBoard.__init__(self, m=self._m, n=self._n, players=2)  # player 1 is the snake, player 2 is the fruit!
+        self.reset()
+
+    @property
+    def is_terminated(self):
+        return (self._snake[0] in self._snake[1:])
+    
+
+    @property
+    def possible_actions(self):
+        '''
+        Returns a list of indexes of empty squares.
+        '''  
+        return ['left', 'none', 'right']
+
+    def take_effect(self, _id, action):
+        self._win.border(0)
+        self._win.addstr(0, 2, 'Score : ' + str(self._score) + ' ')                # Printing 'Score' and
+        self._win.addstr(0, 27, ' SNAKE ')                                   # 'SNAKE' strings
+        self._win.timeout(150 - int(len(self._snake)/5 + len(self._snake)/10)%120)          # Increases the speed of Snake as its length increases
+        
+        # prevKey = self._key                                                  # Previous key pressed
+        _ = self._win.getch()
+        if action == 'left':
+            if self._key == KEY_LEFT:
+                self._key = KEY_DOWN
+            elif self._key == KEY_DOWN:
+                self._key = KEY_RIGHT
+            elif self._key == KEY_RIGHT:
+                self._key = KEY_UP
+            elif self._key == KEY_UP:
+                self._key = KEY_LEFT
+        elif action == 'right':
+            if self._key == KEY_LEFT:
+                self._key = KEY_UP
+            elif self._key == KEY_UP:
+                self._key = KEY_RIGHT
+            elif self._key == KEY_RIGHT:
+                self._key = KEY_DOWN
+            elif self._key == KEY_DOWN:
+                self._key = KEY_LEFT
+
+        # if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
+        #     key = -1                                                   # one (Pause/Resume)
+        #     while key != ord(' '):
+        #         key = win.getch()
+        #     key = prevKey
+        #     continue
+
+        # if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
+        #     key = prevKey
+
+        # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
+        # This is taken care of later at [1].
+        self._snake.insert(0, [self._snake[0][0] + (self._key == KEY_DOWN and 1) + (self._key == KEY_UP and -1), self._snake[0][1] + (self._key == KEY_LEFT and -1) + (self._key == KEY_RIGHT and 1)])
+        if self._snake[0][0] == 0: self._snake[0][0] = self._m - 2
+        if self._snake[0][1] == 0: self._snake[0][1] = self._n - 2
+        if self._snake[0][0] == self._m: self._snake[0][0] = 1
+        if self._snake[0][1] == self._n: self._snake[0][1] = 1
+        # print(self._snake[0][0], self._snake[0][1], (self._snake[0][0] * 6 + self._snake[0][1]))
+        self.set_piece(1, row=self._snake[0][0], column=self._snake[0][1])
+        if self._snake[0] == self._food:                                            # When snake eats the food
+            self._food = []
+            self._score += 1
+            while self._food == []:
+                self._food = [randint(1, self._m-2), randint(1, self._n-2)]                 # Calculating next food's coordinates
+                if self._food in self._snake: self._food = []
+            self._win.addch(self._food[0], self._food[1], '*')
+            self.set_piece(2, row=self._food[0], column=self._food[1])
+        else:
+            last = self._snake.pop()                                          # [1] If it does not eat the food, length decreases
+            self._win.addch(last[0], last[1], ' ')
+            self.clear_piece(row=last[0], column=last[1])
+        self._win.addch(self._snake[0][0], self._snake[0][1], '#')
+        return self._score
+
+    def reset(self):
+        MNKGame.reset(self)
+        curses.initscr()
+        self._win = curses.newwin(20, 60, 0, 0)
+        self._win.keypad(1)
+        curses.noecho()
+        curses.curs_set(0)
+        self._win.border(0)
+        self._win.nodelay(1)
+        self._key = KEY_RIGHT                                                    # Initializing values
+        self._score = 0
+        self._snake = [[self._m // 2, self._n // 2 + 1],
+                       [self._m // 2, self._n // 2],
+                       [self._m // 2, self._n // 2 - 1]]                                     # Initial snake co-ordinates
+        self._food = [self._m // 2 + 1, self._n // 2]                                                     # First food co-ordinates
+        self._win.addch(self._food[0], self._food[1], '*')                       # Prints the food
+        Subject.__init__(self)
+        for location in self._snake:
+            self.set_piece(1, row=location[0], column=location[1])
+        self.set_piece(2, row=self._food[0], column=self._food[1])
+
+    def printable(self):
+        pass
+
+         
 
 if __name__ == '__main__':
     main()
