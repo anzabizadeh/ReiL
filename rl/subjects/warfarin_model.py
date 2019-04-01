05bea256e2ca04097f10d9cce464be348f894d5a
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2019-03-31 21:11:11 -0500

    updated gitignore + started warfarin subject

--- rl/subjects/warfarin_model.py
+++ rl/subjects/warfarin_model.py
@@ -13,8 +13,6 @@ from math import exp
 from ..valueset import ValueSet
 from .subject import Subject
 
-# NOTHING HAS YET IMPLEMENTED!!!!!
-
 
 class WarfarinModel(Subject):
     '''
@@ -35,31 +33,13 @@ class WarfarinModel(Subject):
     def __init__(self, **kwargs):
         Subject.__init__(self, **kwargs)
         Subject.set_defaults(self,
-            drug={'initial_value': 0, 'infusion_rate': 0, 'decay_rate': 0,
-                  'normal_cell_kill_rate': 0, 'tumor_cell_kill_rate': 0, 'immune_cell_kill_rate': 0},
-            normal_cells={'initial_value': 0, 'growth_rate': 0, 'carrying_capacity': 0},
-            tumor_cells={'initial_value': 0, 'growth_rate': 0, 'carrying_capacity': 0},
-            immune_cells={'initial_value': 0, 'influx_rate': 0, 'threshold_rate': 1, 'response_rate': 0, 'death_rate': 0},
-            competition_term={'normal_from_tumor':0, 'tumor_from_normal': 0, 'tumor_from_immune': 0, 'immune_from_tumor': 0},
-            x={'drug': 0, 'normal_cells': 0, 'tumor_cells': 0, 'immune_cells': 0, 'day': 1},
+            Cs_super=0, AGE=60, CYP2C9='*1/*1', VKORC1='A/A', SS=1, maxTime=24, rseed=12345,
+            day=0, state={}, 
+            max_dose=15, dose_steps=0.5,
             state_function=lambda x: {'value': 0, 'min':0, 'max':0}, reward_function=lambda new_x, old_x: 1-new_x['cancer']/old_x['cancer'],
             termination_check= lambda x: x['tumor_cells']==0,
-            # e=lambda x: 0, state_range=[0],
-            u_max=10, u_steps=20)
+            )
         Subject.set_params(self, **kwargs)
-
-        # The following code is just to suppress debugger's undefined variable errors!
-        # These can safely be deleted, since all the attributes are defined using set_params!
-        if False:
-            self._agent_list, self._drug, self._normal_cells, self._tumor_cells, self._immune_cells = {}, {}, {}, {}, {}
-            self._state_function = lambda x: {'value': 0, 'min':0, 'max':0}
-            self._reward_function = lambda new_x, old_x: 1-new_x['cancer']/old_x['cancer']
-            self._termination_check = lambda x: x['tumor_cells']==0
-            # self._e, self._state_range,  = lambda x: 0, [0]
-            self._competition_term, self._u_max, self._u_steps = {}, 10, 20
-
-        self._x = {'drug': self._drug['initial_value'], 'normal_cells': self._normal_cells['initial_value'],
-                   'tumor_cells': self._tumor_cells['initial_value'], 'immune_cells': self._immune_cells['initial_value'], 'day': 1}
 
     @property
     def state(self):
@@ -76,8 +56,8 @@ class WarfarinModel(Subject):
 
     @property
     def possible_actions(self):
-        return ValueSet([self._u_max*x/self._u_steps for x in range(0, self._u_steps+1)], min=0, max=self._u_max, 
-                        binary=lambda x: (int(x * self._u_steps // self._u_max), self._u_steps+1)).as_valueset_array()
+        return ValueSet([self._max_dose*x/self._dose_steps for x in range(0, self._dose_steps+1)], min=0, max=self._max_dose, 
+                        binary=lambda x: (int(x * self._dose_steps // self._max_dose), self._dose_steps+1)).as_valueset_array()
 
     def register(self, agent_name):
         '''
