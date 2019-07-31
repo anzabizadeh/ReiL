fe4f6699b0c06fbfe2053b0d587a54624c2776ba
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2019-07-31 14:57:54 -0500

    Modified register() added deregister()

--- rl/subjects/subject.py
+++ rl/subjects/subject.py
@@ -59,9 +59,17 @@ class Subject(RLBase):
         try:
             return self._agent_list[agent_name]
         except KeyError:
-            _id = len(self._agent_list) + 1
+            _id = max(self._agent_list.values()) + 1
             self._agent_list[agent_name] = _id
             return _id
+
+    def deregister(self, agent_name):
+        '''
+        Deegisters an agent given its name.
+        \nArguments:
+        \n    agent_name: the name of the agent to be registered.
+        '''
+        self._agent_list.pop(agent_name)
 
     def take_effect(self, _id, action):
         raise NotImplementedError
