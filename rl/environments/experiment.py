1dd6984977b0907cb7434e0b535cbe056f56d14d
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2019-08-24 01:14:31 -0500

    Removed trajectory() from Experiment.

--- rl/environments/experiment.py
+++ rl/environments/experiment.py
@@ -76,151 +76,6 @@ class Experiment(Environment):
             self._number_of_subjects, self._max_steps = 1, 10000
             self._save_subjects = 'all'
 
-
-    def elapse(self, **kwargs):
-        '''
-        Move forward in time for a number of episodes.
-
-        At each episode, agents are called sequentially to act on their respective subject(s).
-        An episode ends if one (all) subject(s) terminates.
-        Arguments
-        ---------
-            episodes: number of episodes of run.
-            max_steps: maximum number of steps in each episode (Default = 10,000)
-            termination: when to terminate one episode. (Default = 'any')
-                'any': terminate if any of the subjects is terminated.
-                'all': terminate if all the subjects are terminated.
-            reset: how to reset subjects after each episode. (Default = 'any')
-                'any': reset only the subject that is terminated.
-                'all': reset all subjects.
-            learning_method: how to learn from each episode. (Default = 'every step')
-                'every step': learn after every move.
-                'history': learn after each episode.
-            reporting: what to report. (Default = 'none')
-                'all': prints every move.
-                'none' reports nothing.
-                'important' reports important parts only.
-            tally: count wins of each agent or not. (Default = 'no')
-                'yes': counts the number of wins of each agent.
-                'no': doesn't tally.
-            step_count: count average number of steps in each episode. (Default = 'no')
-                'yes': counts the average number of steps in each episode.
-                'no': doesn't count.
-        '''
-        episodes = kwargs.get('episodes', self._episodes)
-        max_steps = kwargs.get('max_steps', self._max_steps)
-        termination = kwargs.get('termination', self._termination).lower()
-        reset = kwargs.get('reset', self._reset)
-        learning_method = kwargs.get(
-            'learning_method', self._learning_method).lower()
-        reporting = kwargs.get('reporting', 'none').lower()
-
-        tally = kwargs.get('tally', 'no').lower() == 'yes'
-        if tally:
-            win_count = {}
-            for agent in self._agent:
-                win_count[agent] = 0
-
-        step_count = kwargs.get('step_count', 'no').lower() == 'yes'
-
-        if learning_method == 'none':
-            for agent in self._agent.values():
-                agent.status = 'testing'
-        else:
-            for agent in self._agent.values():
-                agent.status = 'training'
-
-        steps = 0
-        for episode in range(episodes):
-            if reporting != 'none':
-                report_string = 'episode: {}'.format(episode+1)
-            # if learning_method == 'history':
-            history = {}
-            for agent_name in self._agent:
-                history[agent_name] = pd.DataFrame(columns=['state', 'action', 'reward'])
-                # history[agent_name] = pd.DataFrame(columns=['state', 'action', 'reward'])
-            done = False
-            stopping_criterion = max_steps * (episode+1)
-            while not done:
-                if steps >= stopping_criterion:
-                    break
-                steps += 1
-                for agent_name, agent in self._agent.items():
-                    if not done:
-                        subject_name, _id = self._assignment_list[agent_name]
-                        subject = self._subject[subject_name]
-                        if not subject.is_terminated:
-                            state = subject.state
-                            possible_actions = subject.possible_actions
-                            action = agent.act(state, actions=possible_actions,
-                                               episode=self._total_experienced_episodes,
-                                               printable=subject.printable())
-                            if reporting == 'all':
-                                print('step: {: 4} episode: {:2} state: {} action: {} by:{}'
-                                      .format(steps, episode, state, action, agent_name))
-                            reward = subject.take_effect(_id, action)
-
-                            history[agent_name].loc[len(history[agent_name].index)] = [state, action, reward]
-
-                            # history[agent_name].append(state)
-                            # history[agent_name].append(action)
-                            # history[agent_name].append(reward)
-
-                            if subject.is_terminated:
-                                if tally & (reward > 0):
-                                    win_count[agent_name] += 1
-                                for affected_agent in self._agent.keys():
-                                    if (self._assignment_list[affected_agent][0] == subject_name) & \
-                                            (affected_agent != agent_name):
-                                        history[affected_agent][-1] = -reward
-
-                    if termination == 'all':
-                        done = True
-                        for sub in self._subject.values():
-                            done = done & sub.is_terminated
-                    elif termination == 'any':
-                        done = False
-                        for sub in self._subject.values():
-                            done = done | sub.is_terminated
-
-                if learning_method == 'every step':
-                    for agent_name, agent in self._agent.items():
-                        state = self._subject[self._assignment_list[agent_name][0]].state
-                        # agent.learn(state=state, reward=history[agent_name][-1]['reward'])
-                        # history[agent_name] = pd.DataFrame(columns=['state', 'action', 'reward'])
-                        agent.learn(state=state, reward=history[agent_name][2])
-                        history[agent_name] = []
-
-            if learning_method == 'history':
-                for agent_name, agent in self._agent.items():
-                    agent.learn(history=history[agent_name])
-
-            if reset == 'all':
-                for agent in self._agent.values():
-                    agent.reset()
-                for subject in self._subject.values():
-                    subject.reset()
-            elif reset == 'any':
-                for agent_name, subject_name in self._assignment_list.items():
-                    if self._subject[subject_name[0]].is_terminated:
-                        self._agent[agent_name].reset()
-                        self._subject[subject_name[0]].reset()
-
-            if tally & (reporting != 'none'):
-                report_string += '\n tally:'
-                for agent in self._agent:
-                    report_string += '\n {} {}'.format(agent, win_count[agent])
-
-            if reporting != 'none':
-                print(report_string)
-
-            self._total_experienced_episodes += 1
-
-        if tally:
-            return win_count
-        if step_count:
-            return steps/episodes
-
     def generate_subjects(self, **kwargs):
         number_of_subjects = kwargs.get('number_of_subjects', self._number_of_subjects)
         self._number_of_subjects = number_of_subjects
@@ -251,7 +106,6 @@ class Experiment(Environment):
         '''
         max_steps = kwargs.get('max_steps', self._max_steps)
         number_of_subjects = kwargs.get('number_of_subjects', self._number_of_subjects)
-        # save_subjects = kwargs.get('save_subjects', self._save_subjects)
         digits = ceil(log10(number_of_subjects))
 
         for agent_name, agent in self._agent.items():
@@ -301,7 +155,7 @@ class Experiment(Environment):
                         except KeyError:
                             pass
 
-                history.to_pickle(output_dir / (agent_name + filename + '.pkl'))
+                history.to_pickle(output_dir / (agent_name + '@' + filename + '.pkl'))
 
     def __repr__(self):
         try:
