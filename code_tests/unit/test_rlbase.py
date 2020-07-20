86c5dfe1bbf64d3e581e0242d49f91e19ddadffa
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2020-07-20 07:13:19 -0500

    Updated the unittest

--- code_tests/unit/test_rlbase.py
+++ code_tests/unit/test_rlbase.py
@@ -5,14 +5,26 @@ from rl.rlbase import RLBase
 
 class testRLData(unittest.TestCase):
     def test_persistent(self):
-        base_1 = RLBase()
+        base_1 = RLBase(name='test_persistent', version=1.0)
         base_1.save(filename='test_persistent')
-        base_2 = RLBase(att_1=100, att_2=200,
-                        persistent_attributes=['att_1', 'att_2'])
+        base_2 = RLBase(name='base_2', version=0.6,
+                        persistent_attributes=['name', 'version'])
         base_2.load(filename='test_persistent')
 
-        self.assertEqual(base_2._att_1, 100)
-        self.assertEqual(base_2._att_2, 200)
+        self.assertEqual(base_2._name, 'base_2')
+        self.assertEqual(base_2._version, 0.6)
+
+    def test_inheritance(self):
+        class interited(RLBase):
+            def __init__(self, myarg: str='hello', **kwargs) -> None:
+                self._myarg = myarg
+                super().__init__(**kwargs)
+
+        test = interited(another_arg=1, name='inherited')
+
+        self.assertEqual(test._name, 'inherited')
+        self.assertIn('_another_arg', test.__dict__)
+
 
 
 if __name__ == "__main__":
