9bbb15cbbb32758feb2945df086e0fd71eec465a
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2020-08-18 02:48:25 -0500

    Added hpc to setup.py

--- setup.py
+++ setup.py
@@ -9,3 +9,13 @@ setup(name='rl',
       license='CC BY-NC-SA 4.0',
       packages=['rl'],
       zip_safe=False)
+
+setup(name='hpc',
+      version='0.1',
+      description='Tools to work with HPC at the University of Iowa',
+      url='https://research-git.uiowa.edu/sanzabizadeh/HPC',
+      author='Sadjad Anzabi Zadeh',
+      author_email='sadjad-anzabizadeh@uiowa.edu',
+      license='CC BY-NC-SA 4.0',
+      packages=['hpc'],
+      zip_safe=False)
