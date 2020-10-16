974575ed400932e0a72ff5d991425ba140567002
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2020-10-16 01:36:03 -0500

    Updated test according to changes in warfarin

--- tests/test_warfarin.py
+++ tests/test_warfarin.py
@@ -2,6 +2,7 @@ import unittest
 
 from reil.subjects import Warfarin
 
+
 class testWarfarin(unittest.TestCase):
     def test_object_creation(self):
         w = Warfarin()
@@ -11,19 +12,24 @@ class testWarfarin(unittest.TestCase):
         interval = [5]
         w = Warfarin(interval=interval)
         for _ in range(5):
-            self.assertEqual(w._get_next_interval(), 5)
+            self.assertEqual(w._current_interval, 5)
+            w._determine_interval_info()
 
         interval = [-5, 2]
         w = Warfarin(interval=interval)
         for _ in range(5):
-            self.assertEqual(w._get_next_interval(), 5)
+            self.assertEqual(w._current_interval, 5)
+            w._determine_interval_info()
 
         w._decision_points_index = 1
         w._decision_points_INR_history[w._decision_points_index] = 2.5
+        w._determine_interval_info()
         for _ in range(5):
-            self.assertEqual(w._get_next_interval(), 2)
+            self.assertEqual(w._current_interval, 2)
+            w._determine_interval_info()
 
         interval = [1, 2, 3, 4, 5]
         w = Warfarin(interval=interval)
         for i in interval:
-            self.assertEqual(w._get_next_interval(), i)
+            self.assertEqual(w._current_interval, i)
+            w._determine_interval_info()
