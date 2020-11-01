625eed39cc4579e7f7a411f1111cc9e3c117e320
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2020-11-01 13:19:52 -0600

    Code cleanup

--- reil/stats/stats.py
+++ reil/stats/stats.py
@@ -1,38 +1,19 @@
-from typing import Any, Dict, Optional, Sequence, Union
+from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
 
 import pandas as pd
 from reil import rlbase
 
 
 class Stats:
-    # def __init__(self, name_stat_dict, all_stats=[], **kwargs):
-    #     '''
-    #     Attributes:
-    #     -----------
-    #         name_stat_dict: a dictionary that assigns a name to groupby, stats, etc. in the form of a nested dictionary.
-    #         Example: name_stat_dict={'stat01': {'stats': 'all', 'groupby': ['some column']}}
-    #     '''
-    #     self._all_stats = all_stats
-    #     self._name_stat_dict = name_stat_dict
-    #     for name in self._name_stat_dict.keys():
-    #         if 'stats' not in self._name_stat_dict[name].keys():
-    #             self._name_stat_dict[name]['stats'] = []
-    #         if 'groupby' not in self._name_stat_dict[name].keys():
-    #             self._name_stat_dict[name]['groupby'] = []
-    #         if self._name_stat_dict[name]['stats'] == 'all':
-    #             self._name_stat_dict[name]['stats'] = self._all_stats
-
     def __init__(self,
-                 all_stats: Sequence = (),
+                 all_stats: Tuple[str, ...] = ('',),
                  active_stats: Union[Sequence[str], str] = 'all',
-                 groupby: Optional[Sequence] = None,
-                 aggregators: Optional[Sequence] = None):
+                 groupby: Optional[List[str]] = None,
+                 aggregators: Optional[List[str]] = None):
         '''
         Attributes:
         -----------
-            active_stats: a list of stats that should be active for calculation.
-                You can also set it to 'all' to indicate that you want all the stats in `all_stats`. 
-            groupby: fields by which in input to stats should be grouped.
+           groupby: fields by which in input to stats should be grouped.
             aggregator: an aggregator function for groupby.
             all_stats: list of all stats.
         '''
