ffd25b410421c6a8d9d4e1163c1d5cc600f8252b
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2020-12-04 20:20:45 -0600

    Documentation/ code improvements

--- reil/utils/mnkboard.py
+++ reil/utils/mnkboard.py
@@ -9,10 +9,8 @@ It serves as a super class for MNKGame.
 @author: Sadjad Anzabi Zadeh (sadjad-anzabizadeh@uiowa.edu)
 '''
 
-import pickle
+from reil.datatypes import ReilData
 
-# from reil.valueset import ValueSet
-from reil.rldata import RLData
 
 def main():
     # create a board and set piece for each player and print the board
@@ -27,81 +25,71 @@ class MNKBoard:
     '''
     Provide an m-by-n board to play.
 
-    Attributes
-    ----------
-        state: the state of the board as a RLData.
+    ### Attributes
+    board_state: the state of the board as a ReilData.
 
-    Methods
-    -------
-        set_piece: set a piece of the specified player on the specified square of the board.
-        clear_square: clear the specified square of the board.
-        get_board: return the board either as a vector or as a matrix.
-        get_action_set: return a list of empty squares.
-        reset: clear the board.
+
+    ### Methods
+    set_piece: set a piece of the specified player on the specified square of the board.
+
+    clear_square: clear the specified square of the board.
+
+    get_board: return the board either as a vector or as a matrix.
+
+    get_action_set: return a list of empty squares.
+
+    reset: clear the board.
     '''
+
     def __init__(self, **kwargs):
         '''
         Initialize an instance of mnkgame.
 
-        Arguments
-        ---------
-            m: number of rows (default=3)
-            n: number of columns (default=3)
-            k: winning criteria (default=3)
-            players: number of players (default=2)
-            can_recapture: whether a piece can be put on an occupied square
-        '''
-        try:  # m
-            self._m = kwargs['m']
-        except KeyError:
-            self._m = 3
-        try:  # n
-            self._n = kwargs['n']
-        except KeyError:
-            self._n = 3
-        try:  # k
-            self._k = kwargs['k']
-        except KeyError:
-            self._k = 3
-        try:  # players
-            self._players = kwargs['players']
-        except KeyError:
-            self._players = 2
-        try:  # players
-            self._can_recapture = kwargs['can_recapture']
-        except KeyError:
-            self._can_recapture = True
+        ### Arguments
+        m: number of rows (default=3)
 
-        self._board = [0]*(self._m*self._n)
+        n: number of columns (default=3)
+
+        k: winning criteria (default=3)
+
+        players: number of players (default=2)
+
+        can_recapture: whether a piece can be put on an occupied square
+        '''
+        self._m = kwargs.get('m', 3)
+        self._n = kwargs.get('n', 3)
+        self._k = kwargs.get('k', 3)
+        self._players = kwargs.get('players', 2)
+        self._can_recapture = kwargs.get('can_recapture', True)
+        self.reset()
 
     def set_piece(self, player, **kwargs):
         '''
         Set a piece for a player.
         
-        Arguments
-        ---------
-            player: ID of the player whose piece will be set on the board.
-            index: If provided, the piece is set using 'index'. Index starts from 0 and assumes the board to be a list.
-            row: The row in which the piece is set.
-            column: The column in which the piece is set.
+        ### Arguments
+        player: ID of the player whose piece will be set on the board.
+
+        index: If provided, the piece is set using 'index'. Index starts from 0 and assumes the board to be a list.
+
+        row: The row in which the piece is set.
+
+        column: The column in which the piece is set.
 
         Raises ValueError if the player or the location is out of range or niether index nor row-column is provided.
         '''
-        try:
-            can_recapture = kwargs['can_recapture']
-        except KeyError:
-            can_recapture = self._can_recapture
+        can_recapture = kwargs.get('can_recapture', self._can_recapture)
 
-        if (player <= 0) | (player > self._players):
+        if player <= 0 or player > self._players:
             raise ValueError('player not found.')
         if not kwargs:
             raise ValueError('No row-column pair or index found.')
         try:
-            if (self._board[kwargs['index']] != 0) & (not can_recapture):
+            if self._board[kwargs['index']] and not can_recapture:
                 raise ValueError('The square is already occupied.')
             self._board[kwargs['index']] = player
         except KeyError:
-            if (self._board[kwargs['row']*self._n + kwargs['column']] != 0) & (not can_recapture):
+            if self._board[kwargs['row'] * self._n + kwargs['column']] and not can_recapture:
                 raise ValueError('The square is already occupied.')
             self._board[kwargs['row']*self._n + kwargs['column']] = player
 
@@ -109,11 +97,12 @@ class MNKBoard:
         '''
         Clear a square on the board.
 
-        Arguments
-        ---------
-            index: If provided, the piece is set using 'index'. Index starts from 0 and assumes the board to be a list.
-            row: The row in which the piece is set.
-            column: The column in which the piece is set.
+        ### Arguments
+        index: If provided, the piece is set using 'index'. Index starts from 0 and assumes the board to be a list.
+
+        row: The row in which the piece is set.
+
+        column: The column in which the piece is set.
 
         Raises ValueError if the player or the location is out of range or niether index nor row-column is provided
         '''
@@ -122,22 +111,21 @@ class MNKBoard:
         try:
             self._board[kwargs['index']] = 0
         except KeyError:
-            self._board[kwargs['row']*self._n + kwargs['column']] = 0
+            self._board[kwargs['row'] * self._n + kwargs['column']] = 0
 
     @property
-    def state(self):
-        ''' Return the state of the board as an RLData.'''
-        return RLData(self._board, lower=0, upper=self._players)
-        # return ValueSet(self._board, min=0, max=self._players)
+    def board_state(self):
+        ''' Return the state of the board as an ReilData.'''
+        return ReilData.single_member(name='state', value=tuple(self._board),
+                                      lower=0, upper=self._players)
 
     def get_board(self, format_='vector'):
         '''
         Return the board.
-        
-        Arguments
-        ---------
-            format_: 'vector' returns the board as a list. 'matrix' returns the board as a 2D list. (Default='vector').
-            
+
+        ### Arguments
+        format_: 'vector' returns the board as a list. 'matrix' returns the board as a 2D list. (Default='vector').
+
         Raises ValueError if undefined format is provided.
         '''
         if format_.lower() == 'vector':
@@ -151,12 +139,11 @@ class MNKBoard:
         '''
         Return a list of indexes of empty squares.
 
-        Arguments
-        ---------
-            format_: 'vector' returns the board as a list. 'matrix' returns the board as a 2D list. (Default='vector').
-            
+        ### Arguments
+        format_: 'vector' returns the board as a list. 'matrix' returns the board as a 2D list. (Default='vector').
+
         Raises ValueError if undefined format is provided.
-        '''  
+        '''
         index = (i for i in range(self._m*self._n) if self._board[i] == 0)
         for action in index:
             if format_.lower() == 'vector':
@@ -171,7 +158,7 @@ class MNKBoard:
         self._board = [0]*(self._m*self._n)
 
     def __str__(self):
-        '''Return a printable format string of the board.''' 
+        '''Return a printable format string of the board.'''
         return ('\n'.join([''.join([f'{item:4}' for item in row])
                            for row in self._matrix()]))
 
@@ -179,9 +166,8 @@ class MNKBoard:
         '''
         Get a board and returns it in 2D list format.
 
-        Arguments
-        ---------
-            args[0]: the board to be converted. If no arguments is supplied, current object's board is returned.
+        ### Arguments
+        args[0]: the board to be converted. If no arguments is supplied, current object's board is returned.
         '''
         if not args:
             vector = self._board
@@ -196,7 +182,9 @@ class MNKBoard:
         return matrix
 
     def __repr__(self):
-        return 'MNKBoard'
+        return (self.__class__.__qualname__ + f', {self._m} x {self._n} board, '
+                f'{self._k} on a line winner, {self._players} players')
+
 
 if __name__ == '__main__':
     main()
