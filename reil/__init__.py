ed2c33eda5a4bcbc1a6b691760fc35b4513c4498
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2020-12-08 09:09:50 -0600

    added `versioneer`

--- reil/__init__.py
+++ reil/__init__.py
@@ -1,48 +1,48 @@
 # -*- coding: utf-8 -*-
 '''
-Reinforcement learning module for Python
-========================================
+A Reinforcement Learning Module for Python
+==========================================
 
 This module provides a framework for training and test of different
 reinforcement learning methods.
 
-submodules
-----------
-agents: objects that learn by acting on one or more subject
-    via an environment and observing the reward.
+Modules
+-------
+agents
+    Entities that act on one or more subject via an environment and observing
+    the reward. `Agents` can learn or just be actors.
 
-subjects: objects with an internal state that get one or
-    more agent's action via environment and return new
-    state and reward.
+subjects
+    Entities with an internal state that get one or more
+    `agents`' action via an `environment` and return new state and reward.
 
-environments: objects that connect agents to subjects and
-    elapse time.
+environments
+    Classes that connect `agents` and `subjects`, and simulate their interactions.
 
-learners: objects that are used as the learner of an `agent`.
+learners
+A set of learning techniques used as the learner of an `agent`.
 
-stats: objects that compute statistics.
+stats
+    Compute statistics.
 
-reilbase: base class for all `reil` objects.
+reilbase
+    Base class for all `reil` classes.
 
-stateful: based class for all stateful objects.
+stateful
+    Based class for all stateful objects.
 
-datatypes: all custom datatypes used in `reil`.
+datatypes
+    All custom datatypes used in `reil`.
 
-utils: all classes that are not part of the agent, subject, environment framework.
+utils
+    Classes and functions that are utilities used in `reil`.
 
-legacy: all classes that are no longer supported.
+legacy
+    All classes that are no longer supported.
 
 @author: Sadjad Anzabi Zadeh (sadjad-anzabizadeh@uiowa.edu)
 '''
 
-
-__all__ = ['agents',
-           'datatypes',
-           'environments',
-           'learners',
-           'legacy',
-           'reilbase',
-           'stateful',
-           'stats',
-           'subjects',
-           'utils']
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
-from setuptools import setup, find_packages
+import setuptools
+import versioneer
 
 with open('README.rst') as f:
-    readme = f.read()
+    long_description = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
-setup(name='Reinforcement Learning',
-      version='0.5.0',
-      description='The reinforcement learning module for Python',
-      long_description=readme,
-      url='https://research-git.uiowa.edu/sanzabizadeh/Reinforcement-Learning',
-      author='Sadjad Anzabi Zadeh',
-      author_email='sadjad-anzabizadeh@uiowa.edu',
-      license=license,
-      packages=find_packages(exclude=('tests', 'docs')))
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(name='ReiL',
+                 version=versioneer.get_version(),
+                 cmdclass=versioneer.get_cmdclass(),
+                 description='A Reinforcement Learning Module for Python',
+                 long_description=long_description,
+                 long_description_content_type="text/x-rst",
+                 url='https://research-git.uiowa.edu/sanzabizadeh/Reinforcement-Learning',
+                 author='Sadjad Anzabi Zadeh',
+                 author_email='sadjad-anzabizadeh@uiowa.edu',
+                 license=license,
+                 packages=setuptools.find_packages(exclude=('tests', 'docs')),
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 python_requires='>=3.7',
+                 install_requires=requirements,
+                 )
