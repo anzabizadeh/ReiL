c8f25f5fa3723b28570d025d127116baab41a4d4
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2021-01-10 02:41:23 -0600

    Modified `load` and how `comp`s are treated.

--- reil/stateful.py
+++ reil/stateful.py
@@ -35,14 +35,12 @@ deregister:
 from __future__ import annotations
 
 import dataclasses
-import functools
 import pathlib
-from reil.datatypes.components import Statistic
-from typing import Any, Dict, List, Optional, Tuple, cast
+from typing import Any, Dict, List, Optional, Tuple, Union, cast
 
 from reil import reilbase
-from reil.datatypes import (PrimaryComponent, ReilData, SecondayComponent,
-                            SubComponentInfo)
+from reil.datatypes import PrimaryComponent, ReilData, SubComponentInfo
+from reil.datatypes.components import Statistic
 
 
 @dataclasses.dataclass
@@ -206,7 +204,7 @@ class Stateful(reilbase.ReilBase):
                          **kwargs)
 
         self.sub_comp_list = self._extract_sub_components()
-        self.state = PrimaryComponent(
+        self.state = PrimaryComponent(self,
             self.sub_comp_list, self._default_state_definition)
         self.statistic = Statistic(
             name='statistic', primary_component=self.state,
@@ -270,28 +268,29 @@ class Stateful(reilbase.ReilBase):
         'new valuenew valuenew value'}
         '''
         sub_comp_list = {}
-        for k, v in self.__class__.__dict__.items():
-            if callable(v) and k[:10] == '_sub_comp_':
-                keywords = list(v.__code__.co_varnames)
-                if 'self' in keywords:
-                    keywords.remove('self')
-                    f = functools.partial(v, self)
-                else:
-                    f = v
+        for func_name, func in self.__class__.__dict__.items():
+            if callable(func) and func_name[:10] == '_sub_comp_':
+                keywords = list(func.__code__.co_varnames)
+                # if 'self' in keywords:
+                #     keywords.remove('self')
+                #     f = functools.partial(v, self)
+                # else:
+                #     f = v
 
                 if 'kwargs' in keywords:
                     keywords.remove('kwargs')
 
-                if len(keywords) == 0 or keywords[0] != '_id':
+                if len(keywords) < 2 or keywords[1] != '_id':
                     raise ValueError(
-                        f'Error in {k} signature: '
-                        'The first argument, except for "self", '
-                        'should be "_id".')
+                        f'Error in {func_name} signature: '
+                        'At least two arguments should be accepted. '
+                        'The first argument will receive a reference to the '
+                        'object (self), and the second argument should be '
+                        '"_id", which is the ID of the caller.')
 
-                if '_id' in keywords:
-                    keywords.remove('_id')
+                keywords.remove('_id')
 
-                sub_comp_list[k[10:]] = (f, tuple(keywords))
+                sub_comp_list[func_name[10:]] = (func, tuple(keywords))
 
         return sub_comp_list
 
@@ -339,260 +338,17 @@ class Stateful(reilbase.ReilBase):
         '''
         self._entity_list.remove(entity_id)
 
+    def load(self, filename: str,
+             path: Optional[Union[str, pathlib.Path]]) -> None:
+        super().load(filename, path=path)
 
-# StateComponentFunction = Callable[...,
-#                                   Union[Dict[str, Any], ReilData]]
-# StateComponentTuple = namedtuple('StateComponentTuple', ('func', 'kwargs'),
-#                                  defaults=({}))
-# ComponentInfo = Union[str, Tuple[str, Dict[str, Any]]]
+        # # Reassign reference to `self` in sub components that use `partial`.
+        # for comp in self.state.sub_components.values():
+        #     if isinstance(comp[0], functools.partial):
+        #         comp[0].__setstate__(
+        #             (comp[0].func, (self, *comp[0].args[1:]),
+        #              comp[0].keywords, None))
 
-# def statistic(self,
-#               name: str = 'default',
-#               _id: Optional[int] = None) -> ReilData:
-#     '''
-#     Return the statistic that caller `_id` has requested, based on the
-#     statistic definition `name`.
-
-#     Arguments
-#     ---------
-#     name:
-#         Name of the statistic definition. If omitted, output of the
-#         `default_statistic` method will be returned.
-
-#     _id:
-#         ID of the caller the retrieves the statistic.
-
-#     Returns
-#     -------
-#     :
-#         The requested statistic.
-#     '''
-#     return self._statistic(name, _id)
-
-
-# def state(self,
-#           name: str = 'default',
-#           _id: Optional[int] = None) -> ReilData:
-#     '''
-#     Return the current state of the instance.
-
-#     Return the state based on the state definition `name`,
-#     and optional `_id` of the caller.
-
-#     Arguments
-#     ---------
-#     name:
-#         Name of the state definition. If omitted, output of the
-#         `default_state` method will be returned.
-
-#     _id:
-#         ID of the agent that calls the state method. In a multi-agent
-#         setting, e.g. an RTS game with fog of war, agents would see the
-#         world differently.
-
-#     Returns
-#     -------
-#     :
-#         State of the instance.
-#     '''
-#     return self._state(name, _id)
-
-# def state(self,
-#           name: str = 'default',
-#           _id: Optional[int] = None) -> ReilData:
-#     if name.lower() == 'default':
-#         return self.default_state(_id)
-
-#     return ReilData([f.func(**f.kwargs)
-#                      for f in self._state_definitions[name.lower()]])
-
-# def add_state_definition(
-#    self, name: str,
-#    component_list: Tuple[ComponentInfo, ...]) -> None:
-#     '''
-#     Add a new state definition.
-
-#     Add a new state definition called `name` with state components
-#     provided in `component_list`.
-
-#     Arguments
-#     ---------
-#     name:
-#         Name of the new state definition.
-
-#     component_list:
-#         A tuple consisting of component information. Each element
-#         in the list should be either (1) name of the component, or (2)
-#         a tuple with the name and a dict of kwargs.
-
-#     Raises
-#     ------
-#     ValueError
-#         if the state already exists.
-#     '''
-#     _name = name.lower()
-#     if _name in self._state_definitions:
-#         raise ValueError(f'State definition {name} already exists.')
-
-#     self._state_definitions[_name] = []
-#     for component in component_list:
-#         if isinstance(component, str):
-#             f = self._available_state_components[component]
-#             kwargs = {}
-#         elif isinstance(component, (tuple, list)):
-#             f = self._available_state_components[component[0]]
-#             kwargs = reilbase.get_argument(component[1], {})
-#         else:
-#             raise ValueError(
-#                 'Items in the component_list should be one of: '
-#                 '(1) name of the component, '
-#                 '(2) a tuple with the name and a dict of kwargs.')
-#         self._state_definitions[_name].append(
-#             StateComponentTuple(f, kwargs))
-
-# def default_state(self, _id: Optional[int] = None) -> ReilData:
-#     '''
-#     Return the default state definition of the subject.
-
-#     Arguments
-#     ---------
-#     _id:
-#         ID of the agent that calls the state method. In a multi-agent
-#         setting, e.g. an RTS game with fog of war, agents would see the world
-#         differently.
-
-#     Returns
-#     -------
-#     :
-#         State of the instance.
-
-#     Notes
-#     -----
-#     `default_state` can be an efficient implementation of the state, compared
-#     to the `state` that composes the state on the fly.
-
-#     `default_state` can be different for different callers. This can be
-#     implemented using `_id`.
-#     '''
-#     return self.complete_state(_id)
-
-# def complete_state(self, _id: Optional[int] = None) -> ReilData:
-#     '''
-#     Return all the information that the subject can provide.
-
-#     Arguments
-#     ---------
-#     _id
-#         ID of the agent that calls the complete_state method.
-
-#     Returns
-#     -------
-#     :
-#         State of the instance.
-
-#     Notes
-#     -----
-#     The default implementation returns all available state components with
-#     their default settings. Based on the state component definition of a
-#     child class, this can include redundant or incomplete information.
-#     '''
-#     return ReilData([f()  # type: ignore
-#                      for f in self._available_state_components.values()])
-
-# def statistic(self,
-#               name: str = 'default',
-#               _id: Optional[int] = None) -> ReilData:
-#     if name.lower() == 'default':
-#         return self.default_statistic(_id)
-
-#     f, s = self._statistic_definitions[name.lower()]
-#     temp = f(self.state(s, _id))
-
-#     return ReilData.single_base(
-#         name='statistic', value=temp)
-
-# def add_statistic_definition(self, name: str,
-#                              rl_function: reil_functions.ReilFunction,
-#                              state_name: str) -> None:
-#     '''
-#     Add a new statistic definition.
-
-#     Add a new statistic definition called `name` with function `rl_function`
-#     that uses state `state_name`.
-
-#     Arguments
-#     ---------
-#     name:
-#         Name of the new statistic definition.
-
-#     rl_function:
-#         An instance of `ReilFunction` that gets the state of the
-#         subject, and computes the statistic. The rl_function should have the
-#         list of arguments from the state in its definition.
-
-#     state_name:
-#         The name of the state definition that should be used to
-#         compute the statistic. ValueError is raise if the state_name is
-#         undefined.
-
-#     Raises
-#     ------
-#     ValueError
-#         if the statistic already exists.
-
-#     ValueError
-#         if the state_name is undefined.
-#     '''
-#     if name.lower() in self._statistic_definitions:
-#         raise ValueError(f'Statistic definition {name} already exists.')
-
-#     if state_name.lower() not in self._state_definitions:
-#         raise ValueError(f'Unknown state name: {state_name}.')
-
-#     self._statistic_definitions[name.lower()] = (rl_function, state_name)
-
-# def default_statistic(self, _id: Optional[int] = None) -> ReilData:
-#     '''
-#     Return the default statistic definition of the subject for caller `_id`.
-
-#     Arguments
-#     ---------
-#     _id:
-#         ID of the agent that calls the reward method.
-
-#     Returns
-#     -------
-#     :
-#         The default statistic.
-#     '''
-#     return ReilData.single_base(
-#         name='default_stat', value=0.0)
-
-# def _generate_state_components(self) -> None:
-#     '''
-#     Generate all state components.
-
-#     Notes
-#     -----
-#     This method should be implemented for all subjects. Each state component
-#     is a function/ method that computes the given state component. The
-#     function can have arguments with default values. It should have
-#     `**kwargs`
-#     arguments to avoid raising exceptions if unnecessary arguments are passed
-#     on to it.
-
-#     Finally, the function should fill a dictionary of state component names
-#     as keys and functions as values.
-
-#     Example
-#     -------
-#     >>> class Dummy(Subject):
-#     ...     some_attribute = None
-#     ...     def _generate_state_components(self) -> None:
-#     ...         def get_some_attribute(**kwargs):
-#     ...             return self.some_attribute
-#     ...         self._available_state_components = {
-#     ...             'some_attribute': get_some_attribute
-#     ...         }
-#     '''
-#     raise NotImplementedError
+        self.state.object_ref = self
+        self.state.set_default_definition(self._default_state_definition)
+        self.statistic.set_default_definition(self._default_statistic_definition)
