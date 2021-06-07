c47de1af454a6aa58768b1d7afad72dde00c6f6f
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2021-06-07 18:17:11 -0500

    new `Com...Parser` and `ConfigParser`

--- reil/utils/__init__.py
+++ reil/utils/__init__.py
@@ -13,14 +13,37 @@ exploration_strategies:
 functions:
     Contains different useful functions.
 
+reil_functions:
+    A set of special objects that are used to define and compute different
+    statistics, rewards, etc.
+
 Classes
 -------
 ActionGenerator:
     A class that accepts categorical and numerical components, and
     generates lists of actions as `FeatureArray` objects.
 
+CategoricalComponent:
+    A class used to define categorical parts of actions.
+
+NumericalComponent:
+    A class used to define numerical parts of actions.
+
 InstanceGenerator:
     Accepts any object derived from `ReilBase`, and generates instances.
+
+CommandlineArgument:
+    A dataclass to define a commandline argument by specifying its name, type
+    and default value.
+
+CommandlineParser:
+    A class to define and parse commandline arguments
+
+ConfigParser:
+    A class to read and parse `YAML` config files.
+
+OutputWriter:
+    A class to write statistics to a `csv` file.
 
 MNKBoard:
     An m-by-n board in which k similar horizontal, vertical, or diagonal
@@ -34,9 +57,11 @@ import reil.utils.functions  # noqa: W0611
 import reil.utils.reil_functions  # noqa: W0611
 
 from .action_generator import (ActionGenerator,  # noqa: W0611
-                               CategoricalComponent,
-                               NumericalComponent)
-
+                               CategoricalComponent, NumericalComponent)
 from .instance_generator import InstanceGenerator  # noqa: W0611
-from .output_writer import OutputWriter  #noqa: W0611
+
+from .argument_parser import (CommandlineArgument,   # noqa: W0611
+                              CommandlineParser, ConfigParser)
+from .output_writer import OutputWriter  # noqa: W0611
+
 from .mnkboard import MNKBoard  # noqa: W0611
---
+++ reil/utils/argument_parser.py
@@ -0,0 +1,91 @@
+import argparse
+import collections
+import dataclasses
+import pathlib
+from typing import Any, Dict, List, Optional, Type, Union
+
+from reil import ReilBase
+from ruamel.yaml import YAML
+
+
+@dataclasses.dataclass
+class CommandlineArgument:
+    name: str
+    type: Type
+    default: Any
+
+
+class CommandlineParser:
+    def __init__(
+            self,
+            cmd_args: List[CommandlineArgument],
+            extra_args: Optional[Dict[str, Any]] = None) -> None:
+
+        self.parsed_args = vars(self._parse_cmd_args(cmd_args, extra_args))
+
+    @staticmethod
+    def _parse_cmd_args(cmd_args: List[CommandlineArgument],
+                        extra_args: Optional[Dict[str, str]] = None
+                        ) -> argparse.Namespace:
+        '''
+        Parse command line arguments, and add `extra_args`.
+        '''
+        arg_parser = argparse.ArgumentParser()
+
+        for arg in cmd_args:
+            temp = {'type': arg.type, 'default': arg.default}
+            if isinstance(arg.default, (list, tuple)):
+                temp['nargs'] = '+'
+            arg_parser.add_argument(f'--{arg.name}', **temp)
+
+        parsed_args = arg_parser.parse_args()
+
+        if extra_args is not None:
+            parsed_args = arg_parser.parse_args(
+                list(b
+                     for a in extra_args.items()
+                     for b in a),
+                namespace=parsed_args)
+
+        return parsed_args
+
+
+class ConfigParser:
+    def __init__(
+            self,
+            config_filenames: Dict[str, str],
+            config_path: Optional[Union[pathlib.Path, str]] = None) -> None:
+
+        if config_filenames:
+            self.config = {
+                key: self._load_config_file(value, config_path)
+                for key, value in config_filenames.items()
+            }
+        else:
+            self.config = collections.defaultdict()
+
+    @staticmethod
+    def _load_config_file(
+            filename: str,
+            path: Optional[Union[pathlib.Path, str]] = None) -> Any:
+
+        _path = pathlib.Path(path or '.')
+        _filename = filename if filename.endswith((
+            '.yaml', '.yml')) else f'{filename}.yaml'
+
+        with open(_path / _filename, 'r') as f:
+            temp = YAML().load(f)
+
+        return temp
+
+    def extract(
+            self, root_name: str, branch_name: str, as_object: bool = False
+    ) -> Dict[str, Any]:
+        conf = self.config[root_name][branch_name]
+        if as_object:
+            return ReilBase.parse_yaml(conf)
+
+        return conf
+
+    def contains(self, root_name: str, branch_name: str) -> bool:
+        return branch_name in self.config[root_name]
