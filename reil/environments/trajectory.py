f066c18015e5ff55893952a91d8fc7d38d801b44
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2021-07-21 14:41:16 -0500

    Replaced `names` with `entities`

--- reil/environments/trajectory.py
+++ reil/environments/trajectory.py
@@ -1,6 +1,7 @@
 import logging
 import pathlib
-from typing import List, Optional, Union
+from typing import Any, Dict, List, Optional, Union
+from reil.agents.agent_demon import AgentDemon
 
 from reil.datatypes import Entity, InteractionProtocol
 from reil.datatypes.feature_array_dumper import FeatureArrayDumper
@@ -8,6 +9,7 @@ from reil.environments.environment_static_map import EnvironmentStaticMap
 from reil.environments.task import Task
 from reil.healthcare.trajectory_dumper import TrajectoryDumper
 from reil.subjects.subject import Subject
+from reil.subjects.subject_demon import SubjectDemon
 from reil.utils import InstanceGenerator
 from reil.utils.argument_parser import ConfigParser
 
@@ -19,21 +21,22 @@ class Trajectory:
             env_path: Optional[Union[pathlib.Path, str]],
             parser: Optional[ConfigParser] = None,
             state_dumper: Optional[FeatureArrayDumper] = None,
-            agent_name: Optional[str] = None,
-            subject_name: Optional[str] = None,
+            agent_entity: Optional[Entity] = None,
+            subject_entity: Optional[Entity] = None,
+            demons: Optional[
+                Dict[str, Union[AgentDemon[Any], SubjectDemon, str]]] = None,
             state_name: Optional[str] = None,
-            action_name: Optional[str] = None,
-            trajectory_name: Optional[str] = None):
+            action_name: Optional[str] = None):
         self._parser = parser
         self._env_filename = env_filename
         self._env_path = env_path
 
         self._state_dumper = state_dumper
-        self._agent_name = agent_name
-        self._subject_name = subject_name
+        self._agent_entity = agent_entity
+        self._subject_entity = subject_entity
+        self._demons = demons
         self._state_name = state_name
         self._action_name = action_name
-        self._trajectory_name = trajectory_name
 
     def run(
             self,
@@ -42,20 +45,21 @@ class Trajectory:
             subject_list: Optional[List[str]] = None,
             subject_save_path: str = '.',
             state_dumper: Optional[FeatureArrayDumper] = None,
-            agent_name: Optional[str] = None,
-            subject_name: Optional[str] = None,
+            agent_entity: Optional[Entity] = None,
+            subject_entity: Optional[Entity] = None,
+            demons: Optional[
+                Dict[str, Union[AgentDemon[Any], SubjectDemon, str]]] = None,
             state_name: Optional[str] = None,
-            action_name: Optional[str] = None,
-            trajectory_name: Optional[str] = None) -> None:
+            action_name: Optional[str] = None) -> None:
 
         subjects: InstanceGenerator[Subject]
         dumper = state_dumper or self._state_dumper
         if isinstance(trajectory_subjects, str):
-            if self._parser:
-                s = self._parser.extract(
-                    'subjects', trajectory_subjects, True)
-            else:
+            if self._parser is None:
                 raise ValueError('parser not found.')
+
+            s = self._parser.extract(
+                'subjects', trajectory_subjects, True)
         else:
             s = trajectory_subjects
 
@@ -81,35 +85,27 @@ class Trajectory:
                 save_path=subject_save_path,
                 state_dumper=dumper)
 
-        _sub_name = subject_name or self._subject_name
-        if _sub_name is None:
-            raise TypeError('subject_name is not specified.')
+        if (_agent_entity := agent_entity or self._agent_entity) is None:
+            raise TypeError('agent_entity is not specified.')
 
-        _agent_name = agent_name or self._agent_name
-        if _agent_name is None:
-            raise TypeError('agent_name is not specified.')
+        if (_subject_entity := subject_entity or self._subject_entity) is None:
+            raise TypeError('subject_entity is not specified.')
 
-        _state_name = state_name or self._state_name
-        if _state_name is None:
+        if (_state_name := state_name or self._state_name) is None:
             raise TypeError('state_name is not specified.')
 
-        _action_name = action_name or self._action_name
-        if _action_name is None:
+        if (_action_name := action_name or self._action_name) is None:
             raise TypeError('action_name is not specified.')
-
-        _tr_name = trajectory_name or self._trajectory_name
-        if _tr_name is None:
-            raise TypeError('trajectory_name is not specified.')
 
         env = EnvironmentStaticMap.from_pickle(
             filename=self._env_filename, path=self._env_path)
-        env.add_entities({_sub_name: subjects})
+        if (_demons := demons or self._demons):
+            env.add_demons(_demons)
+        env.add_entities({_subject_entity.name: subjects})
 
         protocol = InteractionProtocol(
-            agent=Entity(name=_agent_name),
-            subject=Entity(
-                name=_sub_name,
-                trajectory_name=_tr_name),
+            agent=_agent_entity,
+            subject=_subject_entity,
             state_name=_state_name,
             action_name=_action_name,
             reward_name='no_reward',
@@ -118,7 +114,7 @@ class Trajectory:
         t = Task(
             name='trajectory',
             path='../experiments/trajectories',
-            agent_training_triggers={_agent_name: 'none'},
+            agent_training_triggers={_agent_entity.name: 'none'},
             interaction_sequence=(protocol,),
         )
 
