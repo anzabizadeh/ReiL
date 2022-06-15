e4086e901f758c7ed5e3043a96e8401135cb9fd8
sanzabizadeh <sadjad-anzabizadeh@uiowa.edu>
2022-06-15 10:58:47 -0500

    MetricProtocol and two warfarin metrics.

---
+++ reil/utils/metrics.py
@@ -0,0 +1,72 @@
+from abc import abstractmethod
+from typing import List, Protocol, Tuple
+
+from reil.datatypes.feature import FeatureSet
+
+
+class MetricProtocol(Protocol):
+    '''
+    The base class for all `metric` classes. It has the same main
+    functions as tf.keras.Metric, so any keras Metric
+    can be a ReiL metric.
+    '''
+
+    @abstractmethod
+    def __init__(self, name: str, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def update_state(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def result(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def reset_states(self):
+        raise NotImplementedError
+
+
+class PTTRMetric(MetricProtocol):
+
+    def __init__(self, name: str, **kwargs):
+        self.name = name
+        self.reset_states()
+
+    def update_state(self, states: Tuple[FeatureSet, ...]):
+        in_range_list = [
+            2.0 <= state['INR_history'].value[-1] <= 3.0  # type: ignore
+            for state in states
+        ]
+        self.count = len(in_range_list)
+        self.in_range = sum(in_range_list)
+
+    def result(self):
+        return self.in_range / self.count
+
+    def reset_states(self):
+        self.count: int = 0
+        self.in_range: int = 0
+
+
+class INRMetric(MetricProtocol):
+
+    def __init__(self, name: str, **kwargs):
+        self.name = name
+        self.reset_states()
+
+    def update_state(self, states: Tuple[FeatureSet, ...]):
+        INRs: List[float] = [
+            state['INR_history'].value[-1]  # type: ignore
+            for state in states
+        ]
+        self.count += len(INRs)
+        self.INR += sum(INRs)
+
+    def result(self):
+        return self.INR / self.count
+
+    def reset_states(self):
+        self.count: int = 0
+        self.INR = 0.0
