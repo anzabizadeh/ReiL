65eca7b0406663b91ffecbcc79e04f6b4d704e06
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2022-11-29 04:49:51 -0600

    A number of changes:
    Changed `prev_action` to `momentum`.
    
    Implemented action masking in `act()`
    
    Implemented `PPO4Warfarin2Phase` which use a separate actor initially

--- reil/healthcare/agents/ppo_warfarin.py
+++ reil/healthcare/agents/ppo_warfarin.py
@@ -2,8 +2,11 @@ from typing import Any, Literal, Optional, Tuple
 
 import numpy as np
 import tensorflow as tf
+
+from reil.agents.agent import AgentBase, TrainingData
 from reil.agents.proximal_policy_optimization import PPO, PPOLearner
 from reil.datatypes.buffers.buffer import Buffer
+from reil.datatypes.dataclasses import History
 from reil.datatypes.feature import FeatureGeneratorType, FeatureSet
 from reil.utils.metrics import INRMetric, PTTRMetric
 
@@ -13,16 +16,16 @@ class PPO4Warfarin(PPO):
             self, learner: PPOLearner,
             buffer: Buffer[FeatureSet, Tuple[Tuple[int, ...], float, float]],
             reward_clip: Tuple[Optional[float], Optional[float]] = ...,
-            gae_lambda: float = 1, prev_action_weight: float = 0.,
-            prev_action_mode: Literal['most recent', 'carry'] = 'most recent',
+            gae_lambda: float = 1, momentum_coef: float = 0.,
+            momentum_mode: Literal['most recent', 'carry'] = 'most recent',
             **kwargs: Any):
         super().__init__(learner, buffer, reward_clip, gae_lambda, **kwargs)
         self._metrics['PTTR'] = PTTRMetric('PTTR')
         self._metrics['INR'] = INRMetric('INR')
         self._previous_action = [
             tf.zeros(x) for x in self._learner._model._output_lengths]
-        self._prev_action_weight = prev_action_weight
-        self._carry = prev_action_mode == 'carry'
+        self._momentum_coef = momentum_coef
+        self._carry = momentum_mode == 'carry'
 
     def _update_metrics(self, **kwargs: Any) -> None:
         super()._update_metrics(**kwargs)
@@ -43,13 +46,36 @@ class PPO4Warfarin(PPO):
         if training_mode:
             temp = logits
             for i, x in enumerate(self._previous_action):
-                logits[i] += self._prev_action_weight * x
+                logits[i] += self._momentum_coef * x
             self._previous_action = logits if self._carry else temp
-            action_index = [
+
+        mask = list(actions.send('return mask_vector'))
+        mask_index = [
+            [i for i, j in enumerate(m) if j]
+            for m in mask
+        ]
+        masked_logits = [
+            tf.gather(lo, m, axis=1)
+            for lo, m in zip(logits, mask_index)
+        ]
+
+        # for i, l_ in enumerate(masked_logits):
+        #     if not tf.math.count_nonzero(l_):
+        #         masked_logits[i] = tf.ones_like(
+        #             logits[i]) * [j for j, m in enumerate(mask[i]) if not m]
+
+        if training_mode:
+            permissible_action_index = [
                 int(tf.random.categorical(logits=lo, num_samples=1))
-                for lo in logits]
+                for lo in masked_logits]
         else:
-            action_index = [int(np.argmax(lo)) for lo in logits]
+            permissible_action_index = [
+                int(np.argmax(lo)) for lo in masked_logits]
+
+        action_index = [
+            mask_index[i][permissible_action_index[i]]
+            for i, masked_logit in enumerate(masked_logits)
+        ]
 
         if len(action_index) == 1:
             action_index = action_index[0]
@@ -61,3 +87,43 @@ class PPO4Warfarin(PPO):
         self._previous_action = [
             tf.zeros(x) for x in self._learner._model._output_lengths]
         return super().reset()
+
+
+class PPO4Warfarin2Phase(PPO4Warfarin):
+    def __init__(
+        self, init_actor: AgentBase, switch_day: int,
+        init_state_comps: Tuple[str, ...], main_state_comps: Tuple[str, ...],
+        learner: PPOLearner,
+        buffer: Buffer[FeatureSet, Tuple[Tuple[int, ...], float, float]],
+        reward_clip: Tuple[Optional[float], Optional[float]] = ...,
+        gae_lambda: float = 1, momentum_coef: float = 0,
+        momentum_mode: Literal['most recent', 'carry'] = 'most recent',
+        **kwargs: Any
+    ):
+        super().__init__(
+            learner, buffer, reward_clip,
+            gae_lambda, momentum_coef, momentum_mode, **kwargs)
+        self._init_actor = init_actor
+        self._switch_day = switch_day
+        self._init_state_comps = init_state_comps
+        self._main_state_comps = main_state_comps
+
+    def act(
+            self, state: FeatureSet, subject_id: int,
+            actions: FeatureGeneratorType, iteration: int = 0) -> FeatureSet:
+        val = state.value
+        if val['day'] < self._switch_day:  # type: ignore
+            for f in set(val.keys()).difference(self._init_state_comps):
+                state.pop(f)
+
+            action = self._init_actor.act(state, subject_id, actions, iteration)
+            return action
+
+        for f in set(val.keys()).difference(self._main_state_comps):
+            state.pop(f)
+
+        return super().act(state, subject_id, actions, iteration)
+
+    def _prepare_training(self, history: History) -> TrainingData[FeatureSet, int]:
+        temp = [h for h in history if (h.action or {}).get('dose') is None]
+        return super()._prepare_training(temp)
