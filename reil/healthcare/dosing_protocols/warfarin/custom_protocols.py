2a7a524431e69bc209d1a410b6fd76032266a4b2
Sadjad Anzabi Zadeh <sadjad-anzabizadeh@uiowa.edu>
2023-07-05 19:29:00 -0500

    New dosing protocols

--- reil/healthcare/dosing_protocols/warfarin/custom_protocols.py
+++ reil/healthcare/dosing_protocols/warfarin/custom_protocols.py
@@ -19,16 +19,26 @@ class CustomProtocol(dp.DosingProtocol):
     '''
     def __init__(
             self,
-            method: Literal['123'] = '123') -> None:
+            method: Literal[
+                '123', 'joint', 'joint_791',
+                'tandem_812', 'separate_891'] = '123') -> None:
         '''
         Arguments
         ---------
         method:
             ID of the trained models.
         '''
-        self._method: Callable[[dict[str, Any]], float]
+        self._method: Callable[[dict[str, Any]], dp.DosingDecision]
         if method == '123':
             self._method = self.protocol_123
+        elif method == 'Joint':
+            self._method = self.protocol_joint
+        elif method == 'joint_791':
+            self._method = self.protocol_joint_791
+        elif method == 'tandem_812':
+            self._method = self.protocol_tandem_812
+        elif method == 'separate_891':
+            self._method = self.protocol_separate_891
         else:
             raise ValueError(f'Unknown method {method}.\n')
 
@@ -42,10 +52,10 @@ class CustomProtocol(dp.DosingProtocol):
         if day == 1:
             raise ValueError('Protocol starts at day 4.')
 
-        return dp.DosingDecision(self._method(patient), 7), additional_info
+        return self._method(patient), additional_info
 
     @staticmethod
-    def protocol_123(patient: dict[str, Any]) -> float:
+    def protocol_123(patient: dict[str, Any]) -> dp.DosingDecision:
         previous_dose: float = patient['dose_history'][-1]
         previous_INR: float = patient['INR_history'][-1]
 
@@ -56,7 +66,87 @@ class CustomProtocol(dp.DosingProtocol):
         else:
             next_dose = previous_dose * 0.5  # -50%
 
-        return next_dose
+        return dp.DosingDecision(next_dose, 7)
+
+    @staticmethod
+    def protocol_joint(patient: dict[str, Any]) -> dp.DosingDecision:
+        previous_dose: float = patient['dose_history'][-1]
+        previous_INR: float = patient['INR_history'][-1]
+
+        if previous_INR <= 1.72:
+            next_dose = previous_dose * 1.6  # 60%
+            duration = 3
+        elif previous_INR <= 2.24:
+            next_dose = previous_dose * 1.4  # 40%
+            duration = 1
+        elif previous_INR <= 3.28:
+            next_dose = previous_dose * 0.7  # -30%
+            duration = 1
+        else:
+            next_dose = previous_dose * 0.5  # -50%
+            duration = 28
+
+        return dp.DosingDecision(next_dose, duration)
+
+    @staticmethod
+    def protocol_joint_791(patient: dict[str, Any]) -> dp.DosingDecision:
+        previous_dose: float = patient['dose_history'][-1]
+        previous_INR: float = patient['INR_history'][-1]
+
+        if previous_INR <= 1.38:
+            next_dose = previous_dose * 1.9  # 90%
+            duration = 2
+        elif previous_INR <= 1.70:
+            next_dose = previous_dose  # 0%
+            duration = 1
+        elif previous_INR <= 2.50:
+            next_dose = previous_dose * 1.9  # 90%
+            duration = 1
+        elif previous_INR <= 2.93:
+            next_dose = previous_dose * 0.6  # -40%
+            duration = 1
+        else:
+            next_dose = previous_dose * 0.2  # -80%
+            duration = 3
+
+        return dp.DosingDecision(next_dose, duration)
+
+    @staticmethod
+    def protocol_tandem_812(patient: dict[str, Any]) -> dp.DosingDecision:
+        previous_dose: float = patient['dose_history'][-1]
+        previous_INR: float = patient['INR_history'][-1]
+
+        if previous_INR <= 2.56:
+            next_dose = previous_dose * 1.6  # 60%
+        elif previous_INR <= 3.15:
+            next_dose = previous_dose * 0.5  # -50%
+        else:
+            next_dose = previous_dose * 0.3  # -70%
+
+        return dp.DosingDecision(next_dose, 3)
+
+    @staticmethod
+    def protocol_separate_891(patient: dict[str, Any]) -> dp.DosingDecision:
+        previous_dose: float = patient['dose_history'][-1]
+        previous_INR: float = patient['INR_history'][-1]
+
+        if previous_INR <= 1.49:
+            next_dose = previous_dose * 1.8  # 80%
+            duration = 2
+        elif previous_INR <= 2.11:
+            next_dose = previous_dose * 0.9  # -10%
+            duration = 1
+        elif previous_INR <= 2.21:
+            next_dose = previous_dose * 0.8  # -20%
+            duration = 2
+        elif previous_INR <= 2.72:
+            next_dose = previous_dose * 0.4  # -60%
+            duration = 28
+        else:
+            next_dose = previous_dose * 0.2  # -80%
+            duration = 28
+
+        return dp.DosingDecision(next_dose, duration)
 
 
 class CustomFullProtocol(dp.ThreePhaseDosingProtocol):
